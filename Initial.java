c9719ea66a744d5c544b5bc2fbffdc84ba0c520c
rohanverma2711 <rohan.kumar_cs19@gla.ac.in>
2021-11-19 22:50:29 +0530

    new changes

--- Initial.java
+++ Initial.java
@@ -1,7 +1,7 @@
 public class Initial{
     public static void main(String [] args){
         System.out.println("hello");
-        System.out.println("new release");
+        System.out.println("new project release");
     }
    
 }
